79090d1085443f536fb6f8c7dd015bbaabbbe72c
ansabazys <ansabazys@gmail.com>
2025-11-10 10:13:14 +0530

    unwanted import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import AppRoutes from "./routes/UserRoutes";
-import Footer from "./components/layout/Footer";
 import AdminRoutes from "./routes/AdminRoutes";
-import { AuthProvider, useAuth } from "./context/authContext";
+import { AuthProvider } from "./context/authContext";
 import UserRoutes from "./routes/UserRoutes";
 import UserLayout from "./components/layout/UserLayout";
 import AdminLayout from "./components/layout/AdminLayout";
-import ProtectedRoute from "./components/common/ProtectedRoute";
 import { CartProvider } from "./context/CartContext";
 import Landing from "./pages/Landing/Landing";
 import { PageProvider } from "./context/PageContext";
